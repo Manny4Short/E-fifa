d558d2fca5e856a27e84d4580531ce87e578cefb
RobotFights <Emmanuel_au@yahoo.com>
2019-02-10 15:26:52 +0100

    script commit

---
+++ script.js
@@ -0,0 +1,44 @@
+
+function press(){alert("now working")
+
+/*
+  var user = document.getElementByid("username");
+  var pass = document.getElementByid("password");
+
+  var coruser ="test";
+  var corpass ="123";
+
+  if (user.value == coruser) {
+
+      if(pass.value == corpass){
+
+        window.alert("you are logged in as:"+ user.value);
+        window.open(#)
+
+      }
+      else{
+          window.alert("incorrect username or password !");
+      }
+  }
+  else{
+        window.alert("incorrect username or password !");
+  }*/
+}
+
+//code here for the read more botton...
+
+/*
+$(document).ready(function() {
+  $("#toggle").click(function() {
+    var elem = $("#toggle").text();
+    if (elem == "Read More") {
+      //Stuff to do when btn is in the read more state
+      $("#toggle").text("Read Less");
+      $("#text").slideDown();
+    } else {
+      //Stuff to do when btn is in the read less state
+      $("#toggle").text("Read More");
+      $("#text").slideUp();
+    }
+  });
+});*/
