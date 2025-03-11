8c71e497f75a709195edc406b938de2bb9c09c48
Manny4Short <emmanuel_au@yahoo.com>
2025-03-12 00:45:26 +0100

    Updated multiple pages and removed Pictures&Videos directory

--- script.js
+++ script.js
@@ -1,44 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const currentYear = new Date().getFullYear();
+  const footer = document.createElement("div");
+  footer.className = "footer";
+  footer.innerHTML = `<p>&copy; ${currentYear} All rights reserved</p>`;
 
-function press(){alert("now working")
-
-/*
-  var user = document.getElementByid("username");
-  var pass = document.getElementByid("password");
-
-  var coruser ="test";
-  var corpass ="123";
-
-  if (user.value == coruser) {
-
-      if(pass.value == corpass){
-
-        window.alert("you are logged in as:"+ user.value);
-        window.open(#)
-
-      }
-      else{
-          window.alert("incorrect username or password !");
-      }
-  }
-  else{
-        window.alert("incorrect username or password !");
-  }*/
-}
-
-//code here for the read more botton...
-
-/*
-$(document).ready(function() {
-  $("#toggle").click(function() {
-    var elem = $("#toggle").text();
-    if (elem == "Read More") {
-      //Stuff to do when btn is in the read more state
-      $("#toggle").text("Read Less");
-      $("#text").slideDown();
-    } else {
-      //Stuff to do when btn is in the read less state
-      $("#toggle").text("Read More");
-      $("#text").slideUp();
-    }
-  });
-});*/
+  document.body.appendChild(footer);
+});
